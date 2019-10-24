697f8140b0f712acd217e645853bb45835bc6f3e
george_stoian <george_stoian@protonmail.ch>
2019-10-24 17:50:08 +0100

    changed name andadded report file

---
+++ CGS_D3_D1/shell.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include "mymemory.h"
+
+void main(){
+    printf("shell> start\n");
+
+    initialize();
+
+    printmemory();
+    printsegmenttable();
+
+    printf("shell> end\n");
+}
