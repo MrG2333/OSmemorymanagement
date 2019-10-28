cd01bda091c2be30a45199f1c0e39c968eb87ce8
george_stoian <george_stoian@protonmail.ch>
2019-10-28 17:53:08 +0000

    Changed press any to press enter

--- CGS_A5_A1/shell.c
+++ CGS_A5_A1/shell.c
@@ -8,7 +8,7 @@ int main(){
     initialize();
 
 
-    printf("Will allocate memory for 5 pointers.\n Press any character to continue");
+    printf("Will allocate memory for 5 pointers.\n Press ENTER character to continue");
     getchar();
 
    char * ptr1 = (char *) mymalloc(15);
@@ -28,18 +28,18 @@ int main(){
 
     printf("\n\nAllocated memory for 5 pointers.\n\n");
 
-    printf("\nPress any character to continue\n");
+    printf("\nPress ENTER character to continue\n");
     getchar();
 
     printmemory();
     printsegmenttable();
 
-    printf("\n\nFree ptr2 of size 25, display segmenttable and reallocate the memory to a pointer of the same size to demonstrate the propper functioning of mymalloc.\n Press any key to continue.\n\n");
+    printf("\n\nFree ptr2 of size 25, display segmenttable and reallocate the memory to a pointer of the same size to demonstrate the propper functioning of mymalloc.\n Press ENTER key to continue.\n\n");
     getchar();
     myfree(ptr2);
 
     printsegmenttable();
-    printf("\n\n run mymalloc for the same size as the freed pointer.\n Press any key to continue.\n\n");
+    printf("\n\n run mymalloc for the same size as the freed pointer.\n Press ENTER key to continue.\n\n");
     getchar();
 
     char * ptr10 = NULL;
@@ -47,42 +47,42 @@ int main(){
     strcpy(ptr10,"12345678901234");
 
     printsegmenttable();
-    printf("\n\n Observe how mymalloc can reuse memory segments of the same size.\n Press any key to continue.\n\n");
+    printf("\n\n Observe how mymalloc can reuse memory segments of the same size.\n Press ENTER key to continue.\n\n");
     getchar();
 
 
 
-    printf("\n All pointers with the exception of 2 not adjiacent ones will be freed in order to demonstrate the defragmentation and free space segment compresion. \nPress any character to continue");
+    printf("\n All pointers with the exception of 2 not adjiacent ones will be freed in order to demonstrate the defragmentation and free space segment compresion. \nPress ENTER character to continue");
     getchar();
     myfree(ptr10);
     myfree(ptr3);
 
     myfree(ptr5);
 
-    printf("\n\nFreed all pointers except 2 not adjiacent ones(1st and 4th).\nPress any key to continue\n\n");
+    printf("\n\nFreed all pointers except 2 not adjiacent ones(1st and 4th).\nPress ENTER key to continue\n\n");
     getchar();
     printmemory();
     printsegmenttable();
 
-    printf("\n\nWill start defragmenting  the memory.\n Press any key to continue.\n");
+    printf("\n\nWill start defragmenting  the memory.\n Press ENTER key to continue.\n");
     getchar();
     mydefrag(segmenttable);
 
 
-    printf("\n\nDefragmented the memory. Will display the memory and the segmenttable.\nPress any key to continue.\n\n");
+    printf("\n\nDefragmented the memory. Will display the memory and the segmenttable.\nPress ENTER key to continue.\n\n");
     getchar();
     printmemory();
     printsegmenttable();
-    printf("\n\n Observe how the data has been moved in the memory.\nPress any key to continue.\n\n");
+    printf("\n\n Observe how the data has been moved in the memory.\nPress ENTER key to continue.\n\n");
     getchar();
 
     ///check if memory is reused after defrag
-    printf("\n\n Allocated a pointer in order to demonstrate that the free, defragmented memory is reused. \nPress any key to continue\n\n");
+    printf("\n\n Allocated a pointer in order to demonstrate that the free, defragmented memory is reused. \nPress ENTER key to continue\n\n");
     getchar();
     char * ptr6 = (char *) mymalloc(101);
     strcpy(ptr6, "__REUSED___REUSED_____REUSED___REUSED_____REUSED___REUSED_____REUSED___REUSED_____REUSED___REUSED___");
 
-    printf("\n\n Allocation done check memory and segmenttable. \nPress any key to continue\n\n");
+    printf("\n\n Allocation done check memory and segmenttable. \nPress ENTER key to continue\n\n");
     getchar();
 
     printmemory();
